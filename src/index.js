71850c519f2839764cb1bacecc327f6da6178bf1
Shridhar Sharma <shridharkaushik29@gmail.com>
2018-11-01 09:29:42 +0530

    Added Typescript Support

--- src/index.js
+++ src/index.js
@@ -63,12 +63,12 @@ var PhotoSwipeService = /** @class */ (function () {
         }
         var pswpElement = angular.element(container).children(".pswp");
         if (!index && index !== 0 && currentImage) {
-            index = images.indexOf(currentImage);
+            index = _.indexOf(images, currentImage);
         }
         else {
             index = 0;
         }
-        images = images.map(function (image) {
+        var $images = _(images).map(function (image) {
             var img = {};
             if (_.isString(image)) {
                 img.src = image;
@@ -94,7 +94,7 @@ var PhotoSwipeService = /** @class */ (function () {
         }
         _.merge(lgopt, options.photoswipeOptions);
         // @ts-ignore
-        var $lg = new PhotoSwipe(pswpElement[0], PhotoSwipeUI_Default, images, lgopt);
+        var $lg = new PhotoSwipe(pswpElement[0], PhotoSwipeUI_Default, $images, lgopt);
         $lg.init();
         $lg.listen('gettingData', function (index, item) {
             if (item.w < 1 || item.h < 1) { // unknown size
@@ -117,209 +117,6 @@ var PhotoSwipeService = /** @class */ (function () {
 angular.module("ngLightgallery", [])
     .service("$lightGallery", LightGalleryService)
     .service("$photoswipe", PhotoSwipeService)
-    //     .factory('$lightGallery', [
-    //         '$q',
-    //         ($q: IQService) => {
-    //             const service = {};
-    //
-    //             service.open = function (options) {
-    //                 let images = options.images || [];
-    //                 const currentImage = options.currentImage || options.current;
-    //                 const container = options.container || "body";
-    //                 let index = options.index;
-    //
-    //                 if (!index && index !== 0 && currentImage) {
-    //                     index = images.indexOf(currentImage);
-    //                 } else {
-    //                     index = 0;
-    //                 }
-    //
-    //                 images = images.map(function (image) {
-    //                     const img = {};
-    //                     if (_.isString(image)) {
-    //                         img.src = image;
-    //                         img.thumb = image;
-    //                     } else if (_.isObject(image)) {
-    //                         img.src = image.url || image.src;
-    //                         img.thumb = image.thumb || img.src;
-    //                     }
-    //                     return img;
-    //                 })
-    //
-    //                 const $lg = angular.element(container);
-    //
-    //                 const lgopt = {};
-    //                 _.merge(lgopt, options.lgoptions)
-    //
-    //                 lgopt.dynamic = true;
-    //                 lgopt.index = index || 0;
-    //                 lgopt.dynamicEl = images;
-    //
-    //                 if (images.length) {
-    //                     $lg.lightGallery(lgopt)
-    //
-    //                     $lg.one("onCloseAfter.lg", function () {
-    //                         $lg.data('lightGallery').destroy(true)
-    //                     })
-    //                 }
-    //
-    //                 return $lg;
-    //             }
-    //
-    //             return service;
-    //         }
-    //     ])
-    //
-    //     .factory('$photoswipe', ['$q', function ($q) {
-    //         const service = {};
-    //
-    //         service.open = function (options) {
-    //             let images = options.images || [];
-    //             let index = options.index;
-    //             const currentImage = options.currentImage || options.current;
-    //             const container = options.container || "body";
-    //
-    //             if (!angular.element(container).children(".pswp").length) {
-    //
-    //
-    //                 angular.element(container).append(`
-    //
-    // <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
-    //
-    //     <!-- Background of PhotoSwipe.
-    //          It's a separate element as animating opacity is faster than rgba(). -->
-    //     <div class="pswp__bg"></div>
-    //
-    //     <!-- Slides wrapper with overflow:hidden. -->
-    //     <div class="pswp__scroll-wrap">
-    //
-    //         <!-- Container that holds slides.
-    //             PhotoSwipe keeps only 3 of them in the DOM to save memory.
-    //             Don't modify these 3 pswp__item elements, data is added later on. -->
-    //         <div class="pswp__container">
-    //             <div class="pswp__item"></div>
-    //             <div class="pswp__item"></div>
-    //             <div class="pswp__item"></div>
-    //         </div>
-    //
-    //         <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
-    //         <div class="pswp__ui pswp__ui--hidden">
-    //
-    //             <div class="pswp__top-bar">
-    //
-    //                 <!--  Controls are self-explanatory. Order can be changed. -->
-    //
-    //                 <div class="pswp__counter"></div>
-    //
-    //                 <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
-    //
-    //                 <button class="pswp__button pswp__button--share" title="Share"></button>
-    //
-    //                 <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
-    //
-    //                 <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
-    //
-    //                 <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR -->
-    //                 <!-- element will get class pswp__preloader--active when preloader is running -->
-    //                 <div class="pswp__preloader">
-    //                     <div class="pswp__preloader__icn">
-    //                       <div class="pswp__preloader__cut">
-    //                         <div class="pswp__preloader__donut"></div>
-    //                       </div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //
-    //             <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-    //                 <div class="pswp__share-tooltip"></div>
-    //             </div>
-    //
-    //             <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
-    //             </button>
-    //
-    //             <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
-    //             </button>
-    //
-    //             <div class="pswp__caption">
-    //                 <div class="pswp__caption__center"></div>
-    //             </div>
-    //
-    //         </div>
-    //
-    //     </div>
-    //
-    // </div>
-    //
-    // `)
-    //
-    //             }
-    //
-    //             const pswpElement = angular.element(container).children(".pswp");
-    //
-    //
-    //             if (!index && index !== 0 && currentImage) {
-    //                 index = images.indexOf(currentImage);
-    //             } else {
-    //                 index = 0;
-    //             }
-    //
-    //             images = images.map(function (image) {
-    //                 const img = {};
-    //
-    //                 if (_.isString(image)) {
-    //                     img.src = image;
-    //                 } else if (_.isObject(image)) {
-    //                     img.src = image.url || image.src;
-    //                 }
-    //
-    //                 img.w = image.w || image.width || 0;
-    //                 img.h = image.h || image.height || 0;
-    //                 return img;
-    //             })
-    //
-    //             const lgopt = {
-    //                 // history & focus options are disabled on CodePen
-    //                 history: false,
-    //                 focus: false,
-    //                 index: index
-    //             };
-    //
-    //             if (options.targetEvent) {
-    //                 lgopt.getThumbBoundsFn = function (index) {
-    //                     var thumbnail = options.targetEvent.target,
-    //                         pageYScroll =
-    //                             window.pageYOffset || document.documentElement.scrollTop,
-    //                         rect = thumbnail.getBoundingClientRect();
-    //                     return {x: rect.left, y: rect.top + pageYScroll, w: rect.width};
-    //                 }
-    //             }
-    //
-    //             _.merge(lgopt, options.photoswipeOptions)
-    //
-    //             const $lg = new PhotoSwipe(pswpElement[0], PhotoSwipeUI_Default, images, lgopt);
-    //
-    //             $lg.init();
-    //
-    //             $lg.listen('gettingData', function (index, item) {
-    //                 if (item.w < 1 || item.h < 1) { // unknown size
-    //                     var img = new Image();
-    //                     img.onload = function () { // will get size after load
-    //                         item.w = this.width; // set image width
-    //                         item.h = this.height; // set image height
-    //                         $lg.invalidateCurrItems(); // reinit Items
-    //                         $lg.updateSize(true); // reinit Items
-    //                     }
-    //                     img.src = item.src; // let's download image
-    //                 }
-    //             });
-    //
-    //             return $lg;
-    //         }
-    //
-    //         return service;
-    //     }
-    //     ])
-    //
     .directive("viewImage", [
     '$lightGallery',
     '$photoswipe',
